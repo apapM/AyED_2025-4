75db01f2e679e6cf94d2efb4f6ff844b95d40617
apapM <papitanaval@gmail.com>
2025-04-24 16:54:07 -0300

    Add files via upload

---
+++ 4_PAPA_J/1_PC4_J.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int salud = 100;
+            int hambre = 50;
+            int energia = 70;
+            int dia = 1;
+            bool sigueVivo = true;
+            Random rand = new Random();
+
+            while (sigueVivo)
+            {
+                dia = +1;
+                Console.WriteLine("=== Día " + dia + " – ¿Qué querés hacer? === \n\n 1. Buscar comida \n 2. Dormir \n 3. Explorar la isla \n 4. Ver estado del personaje \n 5. Salir del juego");
+                int opcion = int.Parse(Console.ReadLine());
+
+                case(1);
+                    if (opcion == 1)
+                {
+
+                }
+            }
+        
+            
+        }
+    }
+}
